032f68fb523ab83cbff244f73d6524399e7296ed
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2024-12-29 19:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  
+  if (arr.length < 2) {
+    return -Infinity; 
+
+  let max = -Infinity; 
+  let secondMax = -Infinity; 
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max; 
+      max = arr[i]; 
+    } else if (arr[i] > secondMax && arr[i] < max) {
+      secondMax = arr[i]; 
+    }
+  }
+
+  if (secondMax === -Infinity) {
+    return -Infinity;
+  }
+
+  return secondMax;
+}
