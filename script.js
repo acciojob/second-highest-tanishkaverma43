aca099e7c78e9eedcbf92d8a26853ce6b4f31d95
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2024-12-30 20:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 function secondHighest(arr) {
-  
-  if (arr.length < 2) {
-    return -Infinity; 
-  }
-
-  let max = -Infinity; 
-  let secondMax = -Infinity; 
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      secondMax = max; 
-      max = arr[i]; 
-    } else if (arr[i] > secondMax && arr[i] < max) {
-      secondMax = arr[i]; 
+    if (arr.length < 2) {
+        return -Infinity; 
     }
-  }
 
-  if (secondMax === -Infinity) {
-    return -Infinity;
-  }
+    let max = -Infinity; 
+    let secondMax = -Infinity; 
 
-  return secondMax;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            secondMax = max; 
+            max = arr[i]; 
+        } else if (arr[i] > secondMax && arr[i] < max) {
+            secondMax = arr[i]; 
+        }
+    }
+
+    return (secondMax === -Infinity) ? -Infinity : secondMax;
 }
-var arr = [5, 1, 2, 3, 4];
-console.log("The second highest number in the array is: " + secondHighest(arr));
